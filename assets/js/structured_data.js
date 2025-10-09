dde953834101d5cf838bbe3d32b7652ddbc8a50c
jmcdonaldProdDir <jmcdonald.consultant@gmail.com>
2025-10-09 12:54:26 -0400

    Add files via upload

---
+++ assets/js/structured_data.js
@@ -0,0 +1,25 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "ProfessionalService",
+  "name": "Plan Do Launch",
+  "url": "https://plandtolaunch.com",
+  "logo": "https://plandtolaunch.com/logo.png",
+  "image": "https://plandtolaunch.com/og-image.jpg",
+  "description": "Plan Do Launch helps SaaS and compliance teams scale faster with proven onboarding frameworks, playbooks, and lightweight portals.",
+  "address": {
+    "@type": "PostalAddress",
+    "addressLocality": "London",
+    "addressRegion": "Ontario",
+    "addressCountry": "CA"
+  },
+  "founder": {
+    "@type": "Person",
+    "name": "John A. McDonald"
+  },
+  "sameAs": [
+    "https://linkedin.com/in/johnamcdonald",
+    "https://x.com/plandtolaunch"
+  ]
+}
+</script>
